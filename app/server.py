f42d16d037b0810288ffb55fd924662e861397bf
omerfurkankaplan <60521051+omerfurkankaplan@users.noreply.github.com>
2023-05-20 01:54:41 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/cann0uiyobvr26q/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['estetikli', 'estetikli']
+classes = ['estetikli', 'estetiksiz']
 path = Path(__file__).parent
 
 app = Starlette()
