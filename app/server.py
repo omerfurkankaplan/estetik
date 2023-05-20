24a77abcc28d9e2d84e01a571bf73711513bd6ee
omerfurkankaplan <60521051+omerfurkankaplan@users.noreply.github.com>
2023-05-20 11:19:08 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,9 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/4h1kjt51wos43fw/EstetikliEstetiksizResNet50-100EpochValid10CokDataNewTestDataExp.pkl?dl=1'
-export_file_name = 'EstetikliEstetiksizResNet50-100EpochValid10CokDataNewTestDataExp.pkl'
-
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
 classes = ['estetikli', 'estetiksiz']
 path = Path(__file__).parent
 
